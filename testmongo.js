95df4aacaf4436f7794767ee0a6a284905439596
Subin Bista <subin.bista@selu.edu>
2025-04-07 17:27:08 -0500

    Updated URI

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,8 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://subinbista:${process.env.DB_PASSWORD}@cluster0.bwukm5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
